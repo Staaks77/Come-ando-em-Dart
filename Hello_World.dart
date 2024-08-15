7dd3c5eede15b0adc679eb144ad992a417244244
Davi Staaks <staaks77@gmail.com>
2024-08-14 21:57:28 -0300

    Variáveis e listas

--- Hello_World.dart
+++ Hello_World.dart
@@ -1,3 +1,26 @@
 void main() {
+  int idade = 25;
+  double altura = 1.86;
+  double exponencial = 78e6;
+  bool geek = true;
+  bool geek2 = (idade == altura);
+  String frase = "Começamos ";
+  String frase2 = "a aprender Dart!!!";
+  String nome = "Davi";
+  String sobrenome = "Staaks";
+  List<String> linguagens = ["Dart", "Python", "Ruby", "PHP"];
+  List<dynamic> davi = [25, 1.86, "Davi"];
+
   print("Hello World!");
+  print(idade);
+  print(altura);
+  print(exponencial);
+  print(geek);
+  print(geek2);
+  print(frase + frase2);
+  print("Meu nome é $nome $sobrenome,\neu tenho $altura metros de altura e "
+      "tenho ${davi[0]} anos de idade");
+
+  print(linguagens[1]);
+  print(davi);
 }
